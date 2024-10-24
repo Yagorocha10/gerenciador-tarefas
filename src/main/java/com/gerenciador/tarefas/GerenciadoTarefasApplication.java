7f200f9ea4b56c648d5dc6d2d400eacbe334febb
yago rocha <yagodasilvarocha49@gmail.com>
2024-10-24 18:22:57 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/gerenciador/tarefas/GerenciadoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadoTarefasApplication.class, args);
+	}
+
+}
